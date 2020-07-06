911633300da3bd851b351feb8b29aef721766e29
Perriguey <yo44prg@icloud.com>
2020-07-06 23:36:28 +0200

    add animation

--- Assets/script/Door.cs
+++ Assets/script/Door.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Door : MonoBehaviour {
     private void OnTriggerEnter(Collider other) {
         if (other.tag == "Player") {
-            Debug.Log(other.name + " fait plop");
+            GetComponent<Animator>().enabled = true;
         }
     }
 }
