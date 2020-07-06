109f2beeefab4ddb0039c8350c038e6b59f5203b
Perriguey <yo44prg@icloud.com>
2020-07-06 19:19:18 +0200

    deplacement system

---
+++ Assets/script/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField]
+    private float speed = 4f, rot = 80f, curSpeed;
+    void FixedUpdate() {
+
+        if(Input.GetKey(KeyCode.LeftControl)) {
+            curSpeed = speed * 2;
+        } else {
+            curSpeed = speed;
+        }
+
+        transform.Rotate(Vector3.up * rot * Time.fixedDeltaTime * Input.GetAxis("Horizontal"));
+        float goForward = Input.GetAxis("Vertical");
+        if(goForward > 0) {
+            transform.Translate(Vector3.forward * curSpeed * Time.fixedDeltaTime * Input.GetAxis("Vertical"));   
+        }
+    }
+}
