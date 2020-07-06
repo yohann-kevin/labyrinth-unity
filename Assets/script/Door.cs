0f94eba7e3f128b12d0820a0942afdcca8722e87
Perriguey <yo44prg@icloud.com>
2020-07-06 22:54:12 +0200

    add exit door

---
+++ Assets/script/Door.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            Debug.Log(other.name + " fait plop");
+        }
+    }
+}
