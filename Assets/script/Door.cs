4f05ee85579b4ab176927f813ca7dcb71e9cf115
Perriguey <yo44prg@icloud.com>
2020-07-07 00:12:44 +0200

    add key

--- Assets/script/Door.cs
+++ Assets/script/Door.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Door : MonoBehaviour {
+    public bool CanOpen = false;
     private void OnTriggerEnter(Collider other) {
-        if (other.tag == "Player") {
+        if (other.tag == "Player" && CanOpen) {
             GetComponent<Animator>().enabled = true;
         }
     }
---
+++ Assets/script/Key.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour {
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.collider.tag == "Player") {
+            GameObject.Find("DoorExit").GetComponent<Door>().CanOpen = true;
+            Destroy(transform.parent.gameObject);
+        }
+    }
+}
