9c04fb6e638b0599ce88879d11d34e6a6c513df0
Perriguey <yo44prg@icloud.com>
2020-09-06 16:27:25 +0200

    feat : add timeleft

---
+++ Assets/script/Counter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour {
+    [SerializeField]
+    private int startCounter = 60;
+
+    [SerializeField]
+    Text txtCounter;
+
+    void Start() {
+        txtCounter.text = "TimeLeft : " + startCounter;
+        StartCoroutine(Pause());
+    }
+
+    IEnumerator Pause() {
+        while (startCounter > 0) {
+            yield return new WaitForSeconds(1f);
+            startCounter--;
+            txtCounter.text = "TimeLeft : " + startCounter;
+        }
+
+        Debug.Log("You are dead");
+    }
+}
