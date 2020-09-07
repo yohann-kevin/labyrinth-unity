04cc4d1d1b766b1360937c804d0d9cb2b1fc5827
Perriguey <yo44prg@icloud.com>
2020-09-07 18:05:29 +0200

    feat : add next level part 1

--- Assets/script/AfterBurner.cs
+++ Assets/script/AfterBurner.cs
@@ -1,9 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AfterBurner : MonoBehaviour {
+
+    [SerializeField]
+    int LevelToLoad;
+
+    [SerializeField]
+    bool AutoIndex = true;
+
+    private void Start() {
+        if (AutoIndex) {
+            LevelToLoad = SceneManager.GetActiveScene().buildIndex + 1; 
+        }
+        
+    }
     private void OnCollisionEnter(Collision collision) {
-        Debug.Log("plop");
+        SceneManager.LoadScene(LevelToLoad);
     }
 }
