af667c23fa98da59ec3872c76e3d40be9f52b5a0
Perriguey <yo44prg@icloud.com>
2020-09-08 17:56:08 +0200

    feat : add first enemy

---
+++ Assets/script/Enemy1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1 : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.collider.tag == "Player") {
+            GameObject.Find("Player").GetComponent<PlayerController>().GameOver();
+        }
+    }
+}
