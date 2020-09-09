8e23f09178e28f794e4fe3286891a5b0b87dd4e6
Perriguey <yo44prg@icloud.com>
2020-09-09 18:17:17 +0200

    feat : add enemy 2

---
+++ Assets/script/Enemy2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2 : MonoBehaviour {
+
+    [SerializeField]
+    float speed = 3f;
+
+    public Transform target;
+
+    void Update() {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.collider.tag == "Player") {
+            GameObject.Find("Player").GetComponent<PlayerController>().GameOver();
+        }
+    }
+}
---
+++ Assets/script/Return.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Return : MonoBehaviour {
+
+    [SerializeField]
+    Transform myTarget;
+
+    [SerializeField]
+    Enemy2 enemy2;
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Ball") {
+            enemy2.target = myTarget;
+        }
+    }
+}
